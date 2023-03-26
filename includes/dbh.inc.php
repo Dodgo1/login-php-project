77ecf40d868f86ddd08d90ec5c15ba378728ae22
Dodgo1 <91613930+Dodgo1@users.noreply.github.com>
2023-03-26 14:19:08 +0200

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,15 +1,4 @@
 <?php
-// $serverName = "sql108.epizy.com";
-// $dBUserName = "epiz_30363212";
-// $dBPassword = "Q2QYKV3wOMnyQYb";
-// $dBName = "epiz_30363212_site_karol";
-// $conn = mysqli_connect($serverName, $dBUserName, $dBPassword, $dBName);
-// if (!$conn) {
-//   die("Conectionn failed: " . mysqli_connect_error());
-// }
-?>
-
-<?php
 $serverName = "localhost";
 $dBUserName = "root";
 $dBPassword = "";
